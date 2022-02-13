d25e441aca5fad24e8e83942578194c4533a8c27
Damian Dobrowolski <damiandob923@gmail.com>
2022-02-13 13:21:13 +0100

    add endpoint Hello (#1)

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet(Name = "GetHello")]
+        public string Get()
+        {
+            return "Hello Damian";
+        }
+    }
+}
